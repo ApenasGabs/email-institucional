71d1549d9aa498b146c0ec7bb9a3cd6df4445cbb
Apenasgabs <gabers357@gmail.com>
2021-02-22 20:14:27 -0300

    chore: many things

--- script.js
+++ script.js
@@ -2,61 +2,71 @@ const btnLogin = document.querySelector(".btn-login");
 const form = document.querySelector("form");
 
 btnLogin.addEventListener("click", event => {
-  event.preventDefault();
+    event.preventDefault();
 
-  const fields = [...document.querySelectorAll(".input-block input")];
+    const fields = [...document.querySelectorAll(".input-block input")];
 
-  fields.forEach(field => {
-    if (field.value === "") form.classList.add("validate-error");
-  });
-
-  const formError = document.querySelector(".validate-error");
-  if (formError) {
-    formError.addEventListener("animationend", event => {
-      if (event.animationName === "nono") {
-        formError.classList.remove("validate-error");
-      }
+    fields.forEach(field => {
+        if (field.value === "") form.classList.add("validate-error");
     });
-  } else {
-    form.classList.add("form-hide");
-  }
+
+    const formError = document.querySelector(".validate-error");
+    if (formError) {
+        formError.addEventListener("animationend", event => {
+            if (event.animationName === "nono") {
+                formError.classList.remove("validate-error");
+            }
+        });
+    } else {
+        form.classList.add("form-hide");
+    }
 });
 
 form.addEventListener("animationstart", event => {
-  if (event.animationName === "down") {
-    document.querySelector("body").style.overflow = "hidden";
-  }
+    if (event.animationName === "down") {
+        document.querySelector("body").style.overflow = "hidden";
+    }
 });
 
 form.addEventListener("animationend", event => {
-  if (event.animationName === "down") {
-    form.style.display = "none";
-    document.querySelector("body").style.overflow = "none";
-  }
+    if (event.animationName === "down") {
+        form.style.display = "none";
+        document.querySelector("body").style.overflow = "none";
+    }
 });
 
 /* background squares */
 const ulSquares = document.querySelector("ul.squares");
 
 for (let i = 0; i < 11; i++) {
-  const li = document.createElement("li");
+    const li = document.createElement("li");
 
-  const random = (min, max) => Math.random() * (max - min) + min;
+    const random = (min, max) => Math.random() * (max - min) + min;
 
-  const size = Math.floor(random(10, 120));
-  const position = random(1, 99);
-  const delay = random(5, 0.1);
-  const duration = random(24, 12);
+    const size = Math.floor(random(10, 120));
+    const position = random(1, 99);
+    const delay = random(5, 0.1);
+    const duration = random(24, 12);
 
-  li.style.width = `${size}px`;
-  li.style.height = `${size}px`;
-  li.style.bottom = `-${size}px`;
+    li.style.width = `${size}px`;
+    li.style.height = `${size}px`;
+    li.style.bottom = `-${size}px`;
 
-  li.style.left = `${position}%`;
+    li.style.left = `${position}%`;
 
-  li.style.animationDelay = `${delay}s`;
-  li.style.animationDuration = `${duration}s`;
-  li.style.animationTimingFunction = `cubic-bezier(${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()})`;
+    li.style.animationDelay = `${delay}s`;
+    li.style.animationDuration = `${duration}s`;
+    li.style.animationTimingFunction = `cubic-bezier(${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()})`;
 
-  ulSquares.appendChild(li);
+    ulSquares.appendChild(li);
 }
+var part1 = '@alunos' //document.getElementById('ra')
+var part2 = '@alunos'
+var part3 =  '@alunos' //document.getElementById('instituicao')
+var part4 = '.edu.br'
+
+
+var emailInsti = part1 + part2 + part3 + part4 
+
+
+console.log(emailInsti)
